8890bb8832633c76c67e4e6c4a8fda719cb7caa6
William Grant <williamg3575467@gmail.com>
2024-09-18 19:10:37 +0100

    Add basic request functionality to allow sending a source URL to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("")
+
+  const clickHandler = async () => {
+    const res = await axios.post("http://localhost:3000/create", { url: url })
+    console.log(res)
+  }
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setUrl(e.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value={url} onChange={inputChangeHandler} />
+      <button onClick={clickHandler}>
+        submit
+      </button>
     </>
   )
 }
