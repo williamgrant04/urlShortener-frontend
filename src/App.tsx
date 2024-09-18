f043ebc51d3773ac96153b4f11cc1528a0741758
William Grant <williamg3575467@gmail.com>
2024-09-18 19:50:50 +0100

    Add react-router-dom and create basic (empty) routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import axios from "axios"
-import { useState } from "react"
+import React, { useState } from "react"
 
 function App() {
   const [url, setUrl] = useState("")
+  const [shortUrl, setShortUrl] = useState("")
 
   const clickHandler = async () => {
     const res = await axios.post("http://localhost:3000/create", { url: url })
     console.log(res)
+    setUrl("")
+    if (res.status === 200) {
+      setShortUrl(res.data.identifier)
+    }
   }
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value)
     setUrl(e.target.value)
   }
 
   return (
     <>
       <input type="text" value={url} onChange={inputChangeHandler} />
-      <button onClick={clickHandler}>
-        submit
-      </button>
+      <input type="button" value="submit" onClick={clickHandler} />
+      {shortUrl && <p>http://localhost:5173/{shortUrl}</p>}
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/:identifier",
+    element: <div>test</div>
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
