7dd7076ed498121be8eea9414aa75e37d8cf3667
William Grant <williamg3575467@gmail.com>
2024-09-20 11:54:13 +0100

    Add basic validation to url to prevent needless data saved on backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
 import axios from "axios"
 import React, { useState } from "react"
 
-function App() {
+const App = () => {
   const [url, setUrl] = useState("")
   const [shortUrl, setShortUrl] = useState("")
+  const [error, setError] = useState("")
+
+  const validateUrl = (): boolean => {
+    if (!url) {
+      setError("Please enter a URL")
+      return false
+    }
+    if (!url.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)) {
+      setError("Please enter a valid URL")
+      return false
+    }
+
+    setError("") // Just remove the error message if it was shown
+    return true
+  }
 
   const clickHandler = async () => {
-    const res = await axios.post("http://localhost:3000/create", { url: url })
-    console.log(res)
-    setUrl("")
-    if (res.status === 200) {
-      setShortUrl(res.data.identifier)
+    setShortUrl("") // Remove a previous short url if it was shown
+    if (validateUrl()) {
+      const response = await axios.post("http://localhost:3000/create", { url: url })
+      setUrl("")
+      if (response.status === 200) {
+        setShortUrl(response.data.identifier)
+      }
     }
   }
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(e.target.value)
   }
-  // TODO: Add validation for url so that it matches the pattern of a valid url before sending it to backend
-  // TODO: (also add validation on backend)
+
   return (
     <>
       <input type="text" value={url} onChange={inputChangeHandler} />
       <input type="button" value="submit" onClick={clickHandler} />
+      {error && <p>{error}</p>}
       {shortUrl && <p>http://localhost:5173/{shortUrl}</p>}
     </>
   )
