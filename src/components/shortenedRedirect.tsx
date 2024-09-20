57452e4673841d322a014e3b733550933d7230bc
William Grant <williamg3575467@gmail.com>
2024-09-20 14:00:12 +0100

    Add basic style to redirect page (it shouldn't be seen for more than a second anyway)

--- src/components/shortenedRedirect.tsx
+++ src/components/shortenedRedirect.tsx
@@ -1,25 +1,32 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
+import styled from "styled-components"
 
 const shortenedRedirect = (): React.JSX.Element => {
   const [sourceURL, setSourceURL] = useState("")
   const [timesUsed, setTimesUsed] = useState(0)
   useEffect(() => {
     const path = window.location.pathname.split("/")[1] // get the identifier from the url
-    console.log(path)
     axios.get(`http://localhost:3000/${path}`).then((res) => {
-      console.log(res)
       setSourceURL(res.data.source_url)
       setTimesUsed(res.data.click_count)
       window.location.href = res.data.source_url
     })
   }, [])
   return (
-    <div>
+    <Main>
       <h1>Redirecting to {sourceURL}</h1>
       <p>This shortened URL has been used {timesUsed} time{timesUsed == 1 ? "" : "s"}</p>
-    </div>
+    </Main>
   )
 }
 
+const Main = styled.div`
+  font-family: 'Roboto', sans-serif;
+  height: 90vh; // Just to make it slightly off-center
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 export default shortenedRedirect
